d0026b6220e84368a7a79e80eb0dd7bf4d796378
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2023-02-17 01:14:16 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "da5076ca59msh8e7ef397c5def8bp1a8863jsn21625e725a52",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "068f38a54dee0b904e9ce88f5a2ce1d8";
